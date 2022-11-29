941c7e4acdc5414170980a5b96fa9855920c29ea
Arthur Felipe Schmitz <109077446+ArtSchm22@users.noreply.github.com>
2022-11-28 23:39:05 -0300

    finalizando app

--- app/calc.js
+++ app/calc.js
@@ -9,6 +9,23 @@ function calculateDinner(){
     if(totalConta === "" || taxaServico == 0 || isNaN(totalConta) || isNaN(numeroPessoas)){
         return alert("Ops... tem algo errado em seus valores, preencha-os corretamente!")
     }
+
+    if(numeroPessoas === "" || numeroPessoas <= 1){
+        numeroPessoas = 1
+        document.getElementById("each").style.display = "none"
+    } else {
+        document.getElementById("each").style.display = "block"
+    }
+
+    let totalTaxaServico = (totalConta * taxaServico) / numeroPessoas
+    let total = totalTaxaServico + (totalConta / numeroPessoas)
+    
+    total = Math.round(total * 100) / 100
+    total = total.toFixed(2)
+
+
+    document.getElementById("valor-calculado").style.display = "block"
+    document.getElementById("valor-por-cabeca").innerHTML = total
 }
 
 document.getElementById("calcular").onclick = function(){
